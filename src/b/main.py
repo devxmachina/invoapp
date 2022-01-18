4dc593763bd2c50376c94c9c9949efb290cae1bc
dev x <devx@air-i5.local>
2022-01-18 23:47:56 +0900

    20220118

--- src/b/main.py
+++ src/b/main.py
@@ -7,33 +7,50 @@ def write_json(data):
         # f.write(json_string)
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-
-
-
 ## GET, POST, EDIT, DELETE
+
+def user_input():
+    keys = [
+            "id:",
+            "date:",
+            "description:",
+            "month:",
+            "amount:",
+            "payment:",
+            "lateCnt:",
+            "paymentStatus:",
+            "remainDeposit:"
+            ]
+    
+    list_records = []
+
+    while (len(keys)):
+        i = 0
+        list_records += input(keys[i])
+        i += 1
+        return list_records
+    
+
 
 with open('data2.json', 'r', encoding='UTF-8') as json_file:
     data = json.load(json_file)
     temp = data["records"]
+    records = user_input()
+
     y = {
-            "id": "",
-            "date": "09-17-2020",
-            "description": "임대료",
-            "month": "9",
-            "method": "",
-            "amount": "8000000",
-            "payment": "",
-            "lateCnt": "",
-            "paymentStatus": "",
-            "remainDeposit": ""
+            "id": records[0],
+            "date": records[1],
+            "description": records[2],
+            "month": records[3],
+            "method": records[4],
+            "amount": records[5],
+            "payment": records[6],
+            "lateCnt": records[7],
+            "paymentStatus": records[8],
+            "remainDeposit": records[9]
         }
     temp.append(y)
 
-app = FastAPI()
-
-@app.get('/')
-async def root():
-    return 
 
 
 
